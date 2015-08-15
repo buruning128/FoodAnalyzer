f59ae1ef5fd51172f56c5223582ba58799687ef8
alex_hui <hohaixrh@126.com>
2015-08-15 18:36:08 +0800

    add more ui
    Signed-off-by: alex_hui <hohaixrh@126.com>

--- FoodAnalyzer/MyViewController.swift
+++ FoodAnalyzer/MyViewController.swift
@@ -8,16 +8,89 @@
 
 import UIKit
 
-class MyViewController: UIViewController {
-
+class MyViewController: UITableViewController {
+    var listMyCells: NSMutableArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var bundle = NSBundle.mainBundle()
+        let plistPath:String! = bundle.pathForResource("MyCells", ofType: "plist")
+        
+        listMyCells = NSMutableArray(contentsOfFile: plistPath)
+        
+      
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    //返回某个节中的行数
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return listMyCells.count
+       
+        
+    }
+
+    
+    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+  
+            return 5
+     
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {5
+            return 40
+  
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    
+            var headerView = UIView(frame: CGRectMake(0, 0, 100, 5))
+            headerView.backgroundColor = UIColor(red: 239, green: 239, blue: 244, alpha: 1)
+            
+            return headerView
+       
+
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+  
+            let cellIdentifier : String = "MyCellIdentifier"
+            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+            
+            var row = indexPath.row
+            var rowDict:NSDictionary = listMyCells.objectAtIndex(row) as! NSDictionary
+            
+            cell.textLabel?.text = rowDict.objectForKey("title") as? String
+            var imgSrc = rowDict.objectForKey("image") as? String
+
+            cell.imageView?.image = UIImage(named: imgSrc!)
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+          
+            
+            return cell
+            
+            
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 
 
